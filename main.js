0bcfeafe93d0388eee748187d63c19431d4768ef
User-2010 <ergashevf050@gmail.com>
2026-07-14 16:47:33 +0300

    hw1 m3

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const obj = {
+    id: 67,
+    customer: "Blade",
+    items: [
+        {
+            name: 'trainers',
+            price: 300,
+            quantity: 8,
+        },
+        {
+            name: 'shoes',
+            price: 350,
+            quantity: 5,
+        },
+        {
+            name: 'shirts',
+            price: 200,
+            quantity: 10,
+        },
+    ],
+            status: 'delivered'
+};
+
+// console.log(obj);
